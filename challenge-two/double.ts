9af487c611ac574d0e310a8a7304c2ef71259dd2
matthew9510@gmail.com <matthew9510@gmail.com>
2019-07-17 20:08:20 -0700

    Feature: created double() function which takes in a number input and returns that number multiplied by two and imported it and used it in index.ts

---
+++ challenge-two/double.ts
@@ -0,0 +1,4 @@
+/*
+This function takes in a number and returns that number multiplied by two
+*/
+export let double = (num:number) => num*2;
---
+++ challenge-two/index.ts
@@ -0,0 +1,3 @@
+import {double} from './double'
+
+export let result: number = double(15);
