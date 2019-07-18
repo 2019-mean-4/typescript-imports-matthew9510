c3118084b6264e7661697d47e9aecedb42ad0547
matthew9510@gmail.com <matthew9510@gmail.com>
2019-07-17 20:00:29 -0700

    Feature: created helloWorld and imported it in index.ts

---
+++ challenge-one/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+//const { helloWorld } = require('./hello-World');
+var hello_world_1 = require("./hello-world");
+var greeting = hello_world_1.helloWorld();
+exports.greeting = greeting;
---
+++ challenge-one/index.ts
@@ -0,0 +1,6 @@
+//const { helloWorld } = require('./hello-World');
+import {helloWorld} from "./hello-world";
+
+let greeting:string = helloWorld()
+
+export {greeting}
